ceb1e0a3d6bfaabd63c8f805f3b98663ce8a4292
ananfang <ananfang@gmail.com>
2024-04-14 23:04:51 +0000

    Init Chakara

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
+import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: "Pic Log",
-  description: "Capture your words in the pic",
+  description: "Capture your words in a pic",
 };
 
 export default function RootLayout({
@@ -12,7 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Button, Text, VStack } from "@chakra-ui/react";
+
 export default function Home() {
   return (
-    <div>hello world</div>
+    <VStack>
+      <Text fontSize='xl'>Pic Log: Capture your words in a pic</Text>
+      <Button colorScheme='blue' size='xs'>Login</Button>
+    </VStack>
+    
   );
 }
