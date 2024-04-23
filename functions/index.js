8cc973773d96795ee229ebd3d71a5caecbc5fa4c
ananfang <ananfang@gmail.com>
2024-04-23 15:53:50 +0000

    implement generate image

--- functions/index.js
+++ functions/index.js
@@ -9,18 +9,36 @@
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const { initializeApp } = require("firebase-admin/app");
-const { getFirestore } = require("firebase-admin/firestore");
+const { getFirestore, Timestamp } = require("firebase-admin/firestore")
+const { getStorage, getDownloadURL } = require('firebase-admin/storage')
 const { onDocumentCreated } = require("firebase-functions/v2/firestore");
+const { default: OpenAI } = require("openai");
+const dotenv = require('dotenv')
 
+// dotenv
+dotenv.config()
+
+// Firebase
 initializeApp()
 const firestoreDB = getFirestore()
+const storageBucket = getStorage().bucket()
 
+// Gemini
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
 const geminiModel = genAI.getGenerativeModel({ model: "gemini-pro" })
 
+// OpenAI
+const openaiViaHelicone = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    baseURL: 'https://oai.hconeai.com/v1',
+    defaultHeaders: {
+        'Helicone-Auth': `Bearer ${process.env.HELICONE_API_KEY}`
+    }
+})
+
 exports.createpost = onDocumentCreated({
     document: 'users/{userId}/posts/{postId}',
-    timeoutSeconds: 15 * 60
+    timeoutSeconds: 540
 }, async (event) => {
     const snapshot = event.data
     if (!snapshot) {
@@ -45,17 +63,54 @@ Generate an illustration topic within 32 English characters based on above Post 
     try {
         const result = await geminiModel.generateContent(prompt)
         const response = await result.response
-        const text = response.text()
+        const topic = response.text()
 
         const userId = event.params.userId
         const postId = event.params.postId
 
         const postRef = firestoreDB.doc(`users/${userId}/posts/${postId}`)
         await postRef.update({
-            topic: text
+            topic,
+            status: 'hasTopic',
+            updatedAt: Timestamp.now()
         })
 
-        console.log('🤓 Post updated')
+        console.log('🤓 Post\'s topic updated')
+
+        const imagePrompt = `Artistic blend of Minimalism, Impressionism, and Anime, featuring a scene with minimalistic styling, a limited color palette, and a sparse background. Soft, blended Impressionist brushstrokes create a serene, emotive depiction about ${topic}`
+
+        const imageResult = await openaiViaHelicone.images.generate({
+            model: 'dall-e-3',
+            prompt: imagePrompt,
+            response_format: 'b64_json',
+        })
+
+        const imageData = imageResult.data[0]
+        const imageDataRevisedPrompt = imageData?.revised_prompt
+        const imageDataB64 = imageData?.b64_json
+
+        if (!!imageDataB64) {
+            const filename = `${Date.now()}.png`
+            const filePath = `users/${userId}/posts/${postId}/${filename}`
+            const imageDataBuffer = Buffer.from(imageDataB64, 'base64')
+            
+            const fileStorageRef = storageBucket.file(filePath)
+            await fileStorageRef.save(imageDataBuffer, {
+                contentType: 'image/png',
+            })
+
+            
+            const downloadURL = await getDownloadURL(fileStorageRef)
+
+            await postRef.update({
+                imageURL: downloadURL,
+                revisedPrompt: imageDataRevisedPrompt,
+                status: 'hasImage',
+                updatedAt: Timestamp.now()
+            })
+    
+            console.log('🥸 Post\'s image updated')
+        }
     } catch (error) {
         console.log('🚨 Gemini error when the post is created: ', error)
     }
